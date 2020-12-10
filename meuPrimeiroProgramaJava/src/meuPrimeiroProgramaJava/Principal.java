6be17db77460896ba2681c09cb1db90009fa8cec
otakai-impacta <64440216+otakai-impacta@users.noreply.github.com>
2020-12-10 09:11:10 -0300

    Update Principal.java
    Alteração no último comentário.

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -25,7 +25,7 @@ public class Principal {
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		// Saldação específica.
+		// Saldação para o nome lido.
 		System.out.printf("Olá %s!\n", nome);
 
 	}
